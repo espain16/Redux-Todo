d4722ac144ee3364482716e6a4c953ba4c864e86
Elise Spain <elisespain@gmail.com>
2019-06-18 21:33:29 -0700

    reducer setup

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,22 +1,30 @@
-
+//payload refers to the actual data in a redux action object 
 
 const initialState = {
-    todo: []
+    todos: [
+        {
+            id: 1,
+            todo: 'Eat more pizza',
+            completed: false
+        }
+    ]
 }
 
 export default(state = initialState, action) => {
     switch(action.type) {
         case ADD_TODO:
-    
-        return [
-            ...state, 
-            {
-                id: action.id,
-                text: action.text,
-                completed: false
-            }
+         const newTodo= {
+             id: state.todos[state.todos.length - 1].id + 1,
+             todo: action.payload,
+             completed: false
 
-        ]
+         };   
+            return{
+                ...state,
+                todos: [...state.todos, newTodo]
+            }
+        default:
+            return state;
     }   
 } 
 
